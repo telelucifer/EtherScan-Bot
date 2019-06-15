c67ced87a7f90a95cfb34cb5e9ffd6a3545d4d78
Bots.Business <git.sync@bots.business>
2019-06-15 13:05:35 +0000

    Bots.Business export for bot id: 25320

---
+++ commands/_addwallet.js
@@ -0,0 +1,14 @@
+/*CMD
+  command: /addwallet
+  help: 
+  need_reply: 
+  auto_retry_time: 
+
+  <<ANSWER
+
+  ANSWER
+  keyboard: 🔗 Add Wallet
+  aliases: 🔗 add wallet
+CMD*/
+
+Bot.sendMessage ("Ok,Now send me your *ETH Address*")
